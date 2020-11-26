7882cee8236f50b7f08d149d5b86224db8480090
Greg Hawthorne <ghawthorne@me.com>
2020-11-26 08:47:49 +0100

    include try except method

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 # new_key = {new_key:new_value for (index, row) in df.iterrows()}
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+# print(phonetic_dict)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print('Sorry you can only enter letters')
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
